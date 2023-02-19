ec512ec68ba35e17ea5f32e61b793e6b1dfaebab
Dmintiy <dima2000malischew2@mail.ru>
2023-02-19 22:40:14 +0500

    first

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,27 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserServiceImpl userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Vanya", "Ivanov", (byte) 18);
+        userService.saveUser("Kirill", "Petrov", (byte) 19);
+        userService.saveUser("Leo", "Baranov", (byte) 20);
+        userService.saveUser("Timur", "Bond", (byte) 21);
+
+        List<User> userList = userService.getAllUsers();
+        if (userList != null) {
+            userList.forEach(System.out::println);
+        }
+
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,7 +1,11 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
@@ -10,26 +14,65 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void createUsersTable() {
-
+        try {
+            Util.statement.executeUpdate("CREATE TABLE IF NOT EXISTS User (" +
+                           "id int auto_increment primary key," +
+                          "name varchar(30) not null," +
+                          "lastName varchar(30) not null," +
+                          "age smallint not null )");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void dropUsersTable() {
-
+        try {
+            Util.statement.executeUpdate("DROP TABLE IF EXISTS User");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        try {
+            Util.statement.executeUpdate(String.format("INSERT INTO User (name, lastName, age) " +
+                    "value ('%s', '%s', %s)", name, lastName, age));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void removeUserById(long id) {
-
+        try {
+            Util.statement.executeUpdate("DELETE FROM User WHERE id=" + id);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        ResultSet resultSet;
+        List<User> userList = new ArrayList<>();
+        try {
+            resultSet = Util.statement.executeQuery("SELECT * FROM User");
+            while (resultSet.next()) {
+                userList.add(
+                        new User(
+                                resultSet.getString(2),
+                                resultSet.getString(3),
+                                (byte) resultSet.getInt(4)));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return userList;
     }
 
     public void cleanUsersTable() {
-
+        try {
+            Util.statement.executeUpdate("TRUNCATE TABLE User");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
